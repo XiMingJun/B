1b6664f734b3286c2dc1189ae35e69b659e78482
wangjian <wangjian@qhfax.com.cn>
2017-04-26 11:50:24 +0800

    创建target文件

---
+++ B/Target_B.h
@@ -0,0 +1,13 @@
+//
+//  Target_B.h
+//  B
+//
+//  Created by wangjian on 2017/4/26.
+//  Copyright © 2017年 com.qhfax. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Target_B : NSObject
+
+@end
---
+++ B/Target_B.m
@@ -0,0 +1,13 @@
+//
+//  Target_B.m
+//  B
+//
+//  Created by wangjian on 2017/4/26.
+//  Copyright © 2017年 com.qhfax. All rights reserved.
+//
+
+#import "Target_B.h"
+
+@implementation Target_B
+
+@end
